568a183b88088a2fb3e4f1ab162a1a11580081b0
Mahim <mahim.cse.uu@outlook.com>
2023-08-07 12:56:19 +0600

    done primary works

--- Surah_player/main.py
+++ Surah_player/main.py
@@ -6,3 +6,16 @@ import os
 player = tkr.Tk()
 player.title("Al-Quran audiobook")
 player.geometry("450 X 350")
+
+directory = askdirectory()
+os.chdir(directory)
+surah_list = os.listdir()
+playlist = tkr.Listbox(player, font="Helvetica 12 bold", bg="yellow", selectmode=tkr.SINGLE)
+
+for surah in surah_list:
+    position = 0
+    playlist.insert(position, surah)
+    position += 1
+
+pygame.init()
+pygame.mixer.init()
